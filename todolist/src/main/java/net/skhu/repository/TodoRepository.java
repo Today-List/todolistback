970b323860bd6d262e30fd66fe0d802642b6e7b9
홍석현 <114850357+fr35wo@users.noreply.github.com>
2023-05-29 21:32:47 +0900

    Delete todolist directory

--- todolist/src/main/java/net/skhu/repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package net.skhu.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.skhu.entity.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Integer>{
-
-}
